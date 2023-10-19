acab407e67448986b2e532cf9d90b1ad953ea2f9
cosmindev <cosmin.tudor82@gmail.com>
2023-10-19 18:04:14 +0300

    Fix NFW new resources - downgrade the provider version

--- examples/simple-no_vcn-oci-native-l7-lbaas-example/provider.tf
+++ examples/simple-no_vcn-oci-native-l7-lbaas-example/provider.tf
@@ -1,21 +1,40 @@
-# Copyright (c) 2020 Oracle and/or its affiliates.
+# Copyright (c) 2023, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-terraform {
-  required_version = "< 1.3.0"
-  required_providers {
-    oci = {
-      source = "oracle/oci"
-    }
-  }
-  experiments = [module_variable_optional_attrs]
+provider "oci" {
+  region               = var.region
+  tenancy_ocid         = var.tenancy_ocid
+  user_ocid            = var.user_ocid
+  fingerprint          = var.fingerprint
+  private_key_path     = var.private_key_path
+  private_key_password = var.private_key_password
 }
 
-provider "oci" {
-  version          = ">= 4.109.0"
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
-  region           = var.region
+provider "oci-home" {
+  alias                = "home"
+  region               = local.regions_map[local.home_region_key]
+  tenancy_ocid         = var.tenancy_ocid
+  user_ocid            = var.user_ocid
+  fingerprint          = var.fingerprint
+  private_key_path     = var.private_key_path
+  private_key_password = var.private_key_password
 }
+
+terraform {
+  required_version = ">= 1.2.0, < 1.3.0"
+
+  required_providers {
+    oci-home = {
+      source                = "oracle/oci"
+      version               = ">= 4.109.0"
+      configuration_aliases = [oci-home.home]
+    }
+    oci = {
+      source                = "oracle/oci"
+      version               = "<= 5.16.0"
+      configuration_aliases = [oci]
+    }
+  }
+
+  experiments = [module_variable_optional_attrs]
+} 
