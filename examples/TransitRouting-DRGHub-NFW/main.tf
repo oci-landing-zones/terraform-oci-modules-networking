2d537b18aeab6d54d8551653bc4fb4738ca2f5df
Cosmin Tudor <cosmin.tudor82@gmail.com>
2023-11-21 14:45:14 +0200

    Dev example transit routing drg hub nfw (#16)
    * Add example: TransitRouting-DRGHub-NFW
    
    * Update Examples

---
+++ examples/TransitRouting-DRGHub-NFW/main.tf
@@ -0,0 +1,15 @@
+# ####################################################################################################### #
+# Copyright (c) 2023 Oracle and/or its affiliates,  All rights reserved.                                  #
+# Licensed under the Universal Permissive License v 1.0 as shown at https: //oss.oracle.com/licenses/upl. #
+# Author: Cosmin Tudor                                                                                    #
+# Author email: cosmin.tudor@oracle.com                                                                   #
+# Last Modified: Wed Nov 15 2023                                                                          #
+# Modified by: Cosmin Tudor, email: cosmin.tudor@oracle.com                                               #
+# ####################################################################################################### #
+
+module "terraform_oci_networking" {
+  source = "../../"
+
+  network_configuration = var.network_configuration
+}
+
