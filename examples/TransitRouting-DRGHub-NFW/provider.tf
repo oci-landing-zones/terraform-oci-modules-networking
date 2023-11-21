11dc040bf329fb2511896a35976cc1bdf1a2016a
Cosmin Tudor <cosmin.tudor82@gmail.com>
2023-11-21 14:45:14 +0200

    Dev example transit routing drg hub nfw (#16)
    * Add example: TransitRouting-DRGHub-NFW
    
    * Update Examples

---
+++ examples/TransitRouting-DRGHub-NFW/provider.tf
@@ -0,0 +1,31 @@
+# ####################################################################################################### #
+# Copyright (c) 2023 Oracle and/or its affiliates,  All rights reserved.                                  #
+# Licensed under the Universal Permissive License v 1.0 as shown at https: //oss.oracle.com/licenses/upl. #
+# Author: Cosmin Tudor                                                                                    #
+# Author email: cosmin.tudor@oracle.com                                                                   #
+# Last Modified: Wed Nov 15 2023                                                                          #
+# Modified by: Cosmin Tudor, email: cosmin.tudor@oracle.com                                               #
+# ####################################################################################################### #
+
+provider "oci" {
+  region               = var.region
+  tenancy_ocid         = var.tenancy_ocid
+  user_ocid            = var.user_ocid
+  fingerprint          = var.fingerprint
+  private_key_path     = var.private_key_path
+  private_key_password = var.private_key_password
+}
+
+terraform {
+  required_version = ">= 1.2.0, < 1.3.0"
+
+  required_providers {
+    oci = {
+      source                = "oracle/oci"
+      version               = "<= 5.16.0"
+      configuration_aliases = [oci]
+    }
+  }
+
+  experiments = [module_variable_optional_attrs]
+} 
