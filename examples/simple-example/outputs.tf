d446fe5dda22ae7050a44fbb982566224c7008ec
cosmindev <cosmin.tudor82@gmail.com>
2023-12-12 11:25:09 +0200

    flat map of provisioned networking resources - to facilitate the integration with other networking modules via network dependency mechanism

--- examples/simple-example/outputs.tf
+++ examples/simple-example/outputs.tf
@@ -3,7 +3,7 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https: //oss.oracle.com/licenses/upl. #
 # Author: Cosmin Tudor                                                                                    #
 # Author email: cosmin.tudor@oracle.com                                                                   #
-# Last Modified: Wed Nov 15 2023                                                                          #
+# Last Modified: Tue Dec 12 2023                                                                          #
 # Modified by: Cosmin Tudor, email: cosmin.tudor@oracle.com                                               #
 # ####################################################################################################### #
 
