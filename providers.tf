755c2bbebc3c2afe7792588ada7e268f34ad0b74
cosmindev <cosmin.tudor82@gmail.com>
2023-12-14 10:09:55 +0200

    Add support for network dependency and multi region network topologies

--- providers.tf
+++ providers.tf
@@ -3,18 +3,16 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https: //oss.oracle.com/licenses/upl. #
 # Author: Cosmin Tudor                                                                                    #
 # Author email: cosmin.tudor@oracle.com                                                                   #
-# Last Modified: Wed Nov 15 2023                                                                          #
+# Last Modified: Wed Dec 13 2023                                                                          #
 # Modified by: Cosmin Tudor, email: cosmin.tudor@oracle.com                                               #
 # ####################################################################################################### #
 
-
 terraform {
-  required_version = ">= 1.2.0, < 1.3.0"
-
+  required_version = "< 1.4.0"
   required_providers {
     oci = {
-      source  = "oracle/oci"
-      version = "<= 5.16.0"
+      source                = "oracle/oci"
+      version               = "<= 5.16.0"
     }
   }
   experiments = [module_variable_optional_attrs]
