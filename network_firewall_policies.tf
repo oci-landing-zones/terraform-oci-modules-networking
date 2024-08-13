e0497106a7a2318544b56b3060c3969b842ec781
Andre Correa <andre.correa@oracle.com>
2024-08-13 17:41:02 -0300

    feat: NFW policy upgrade - initial

--- network_firewall_policies.tf
+++ network_firewall_policies.tf
@@ -1,12 +1,5 @@
-# ####################################################################################################### #
-# Copyright (c) 2023 Oracle and/or its affiliates,  All rights reserved.                                  #
-# Licensed under the Universal Permissive License v 1.0 as shown at https: //oss.oracle.com/licenses/upl. #
-# Author: Cosmin Tudor                                                                                    #
-# Author email: cosmin.tudor@oracle.com                                                                   #
-# Last Modified: Wed Nov 15 2023                                                                          #
-# Modified by: Cosmin Tudor, email: cosmin.tudor@oracle.com                                               #
-# ####################################################################################################### #
-
+# Copyright (c) 2023 Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 locals {
   one_dimension_processed_nfw_policies = local.one_dimension_processed_non_vcn_specific_gateways != null ? {
@@ -27,7 +20,7 @@ locals {
           defined_tags                   = nfwp_value.defined_tags
           display_name                   = nfwp_value.display_name
           freeform_tags                  = nfwp_value.freeform_tags
-          application_lists              = nfwp_value.application_lists
+          applications                   = nfwp_value.applications
           decryption_profiles            = nfwp_value.decryption_profiles
           decryption_rules               = nfwp_value.decryption_rules
           ip_address_lists               = nfwp_value.ip_address_lists
@@ -40,27 +33,87 @@ locals {
     ]) : flat_nfwp.nfwp_key => flat_nfwp
   } : null
 
+  nfw_policy_applications = flatten([
+    for policy_key, policy_value in coalesce(local.one_dimension_processed_nfw_policies,{}) : [
+      for app_key, app_value in (coalesce(policy_value.applications,{})) : {
+        key        = "${policy_key}.${app_key}"
+        policy_key = policy_key
+        name       = app_value.name
+        type       = app_value.type
+        icmp_type  = app_value.icmp_type
+        icmp_code  = app_value.icmp_code
+      }
+    ]
+  ])
+
+  nfw_policy_decryption_profiles = flatten([
+    for policy_key, policy_value in coalesce(local.one_dimension_processed_nfw_policies,{}) : [
+      for prof_key, prof_value in (coalesce(policy_value.decryption_profiles,{})) : {
+        key        = "${policy_key}.${prof_key}"
+        policy_key = policy_key
+        type = prof_value.type
+        name = prof_value.name
+        is_out_of_capacity_blocked            = prof_value.is_out_of_capacity_blocked
+        is_unsupported_cipher_blocked         = prof_value.is_unsupported_cipher_blocked
+        is_unsupported_version_blocked        = prof_value.is_unsupported_version_blocked
+        are_certificate_extensions_restricted = prof_value.are_certificate_extensions_restricted
+        is_auto_include_alt_name              = prof_value.is_auto_include_alt_name
+        is_expired_certificate_blocked        = prof_value.is_expired_certificate_blocked
+        is_revocation_status_timeout_blocked  = prof_value.is_revocation_status_timeout_blocked
+        is_unknown_revocation_status_blocked  = prof_value.is_unknown_revocation_status_blocked
+        is_untrusted_issuer_blocked           = prof_value.is_untrusted_issuer_blocked
+      }
+    ]
+  ])
+
+  nfw_policy_address_lists = flatten([
+    for policy_key, policy_value in coalesce(local.one_dimension_processed_nfw_policies,{}) : [
+      for add_key, add_value in (coalesce(policy_value.ip_address_lists,{})) : {
+        key        = "${policy_key}.${add_key}"
+        policy_key = policy_key
+        name       = add_value.name
+        type       = add_value.type
+        addresses  = add_value.addresses
+      }
+    ]
+  ])
+
+  nfw_policy_decryption_rules = flatten([
+    for policy_key, policy_value in coalesce(local.one_dimension_processed_nfw_policies,{}) : [
+      for rule_key, rule_value in (coalesce(policy_value.decryption_rules,{})) : {
+        key        = "${policy_key}.${rule_key}"
+        policy_key = policy_key
+        action     = rule_value.action
+        name       = rule_value.name
+        secret     = rule_value.secret
+        decryption_profile_id       = rule_value.decryption_profile_id
+        destination_ip_address_list = rule_value.destination_ip_address_list
+        source_ip_address_list      = rule_value.source_ip_address_list
+      }
+    ]
+  ])
+
   provisioned_oci_network_firewall_network_firewall_policies = {
     for nfw_pol_key, nfw_pol_value in oci_network_firewall_network_firewall_policy.these : nfw_pol_key => {
-      application_lists              = nfw_pol_value.application_lists
+      #application_lists              = nfw_pol_value.application_lists
       compartment_id                 = nfw_pol_value.compartment_id
-      decryption_profiles            = nfw_pol_value.decryption_profiles
-      decryption_rules               = nfw_pol_value.decryption_rules
+      #decryption_profiles            = nfw_pol_value.decryption_profiles
+      #decryption_rules               = nfw_pol_value.decryption_rules
       defined_tags                   = nfw_pol_value.defined_tags
       display_name                   = nfw_pol_value.display_name
       freeform_tags                  = nfw_pol_value.freeform_tags
       id                             = nfw_pol_value.id
-      ip_address_lists               = nfw_pol_value.ip_address_lists
-      is_firewall_attached           = nfw_pol_value.is_firewall_attached
+      #ip_address_lists               = nfw_pol_value.ip_address_lists
+      #is_firewall_attached           = nfw_pol_value.is_firewall_attached
       lifecycle_details              = nfw_pol_value.lifecycle_details
-      mapped_secrets                 = nfw_pol_value.mapped_secrets
-      security_rules                 = nfw_pol_value.security_rules
+      #mapped_secrets                 = nfw_pol_value.mapped_secrets
+      #security_rules                 = nfw_pol_value.security_rules
       state                          = nfw_pol_value.state
       system_tags                    = nfw_pol_value.system_tags
       time_created                   = nfw_pol_value.time_created
       time_updated                   = nfw_pol_value.time_updated
       timeouts                       = nfw_pol_value.timeouts
-      url_lists                      = nfw_pol_value.url_lists
+      #url_lists                      = nfw_pol_value.url_lists
       network_configuration_category = local.one_dimension_processed_nfw_policies[nfw_pol_key].network_configuration_category
       nfwp_key                       = nfw_pol_key
     }
@@ -69,6 +122,98 @@ locals {
 
 
 resource "oci_network_firewall_network_firewall_policy" "these" {
+  for_each       = local.one_dimension_processed_nfw_policies != null && length(local.one_dimension_processed_nfw_policies) > 0 ? local.one_dimension_processed_nfw_policies : {}
+    compartment_id = each.value.compartment_id != null ? (length(regexall("^ocid1.*$", each.value.compartment_id)) > 0 ? each.value.compartment_id : var.compartments_dependency[each.value.compartment_id].id) : null
+    defined_tags   = each.value.defined_tags
+    display_name   = each.value.display_name
+    freeform_tags  = merge(local.cislz_module_tag, each.value.freeform_tags)
+}
+
+resource "oci_network_firewall_network_firewall_policy_application" "these" {
+  for_each = { for v in local.nfw_policy_applications : v.key => {
+                                                            policy_key = v.policy_key
+                                                            name       = v.name
+                                                            type       = v.type
+                                                            icmp_code  = v.icmp_code
+                                                            icmp_type  = v.icmp_type
+                                                         } }
+    network_firewall_policy_id = oci_network_firewall_network_firewall_policy.these[each.value.policy_key].id
+    name      = each.value.name
+    type      = each.value.type
+    icmp_type = each.value.icmp_type
+    icmp_code = each.value.icmp_code
+}
+
+resource "oci_network_firewall_network_firewall_policy_decryption_profile" "these" {
+  for_each = { for v in local.nfw_policy_decryption_profiles : v.key => {
+                                                            policy_key = v.policy_key
+                                                            type = v.type
+                                                            name = v.name
+                                                            is_out_of_capacity_blocked            = v.is_out_of_capacity_blocked
+                                                            is_unsupported_cipher_blocked         = v.is_unsupported_cipher_blocked
+                                                            is_unsupported_version_blocked        = v.is_unsupported_version_blocked
+                                                            are_certificate_extensions_restricted = v.are_certificate_extensions_restricted
+                                                            is_auto_include_alt_name              = v.is_auto_include_alt_name
+                                                            is_expired_certificate_blocked        = v.is_expired_certificate_blocked
+                                                            is_revocation_status_timeout_blocked  = v.is_revocation_status_timeout_blocked
+                                                            is_unknown_revocation_status_blocked  = v.is_unknown_revocation_status_blocked
+                                                            is_untrusted_issuer_blocked           = v.is_untrusted_issuer_blocked
+                                                         } }
+    network_firewall_policy_id = oci_network_firewall_network_firewall_policy.these[each.value.policy_key].id
+    name = each.value.name
+    type = each.value.type # Valid values: "SSL_FORWARD_PROXY", "SSL_INBOUND_INSPECTION"
+    # Optional attributes
+    is_out_of_capacity_blocked            = each.value.is_out_of_capacity_blocked
+    is_unsupported_cipher_blocked         = each.value.is_unsupported_cipher_blocked
+    is_unsupported_version_blocked        = each.value.is_unsupported_version_blocked
+    are_certificate_extensions_restricted = each.value.are_certificate_extensions_restricted
+    is_auto_include_alt_name              = each.value.is_auto_include_alt_name
+    is_expired_certificate_blocked        = each.value.is_expired_certificate_blocked
+    is_revocation_status_timeout_blocked  = each.value.is_revocation_status_timeout_blocked
+    is_unknown_revocation_status_blocked  = each.value.is_unknown_revocation_status_blocked
+    is_untrusted_issuer_blocked           = each.value.is_untrusted_issuer_blocked
+}
+
+resource "oci_network_firewall_network_firewall_policy_address_list" "these" {
+  for_each = { for v in local.nfw_policy_address_lists : v.key => {
+                                                            policy_key = v.policy_key
+                                                            name       = v.name
+                                                            type       = v.type
+                                                            addresses  = v.addresses
+                                                         } }
+    network_firewall_policy_id = oci_network_firewall_network_firewall_policy.these[each.value.policy_key].id
+    name      = each.value.name
+    type      = each.value.type
+    addresses = each.value.addresses
+}
+
+resource "oci_network_firewall_network_firewall_policy_decryption_rule" "these" {
+  for_each = { for v in local.nfw_policy_decryption_rules : v.key => {
+                                                            policy_key = v.policy_key
+                                                            name       = v.name
+                                                            action     = v.action
+                                                            secret     = v.secret
+                                                            decryption_profile_id = v.decryption_profile_id
+                                                            destination_ip_address_list = v.destination_ip_address_list
+                                                            source_ip_address_list      = v.source_ip_address_list
+                                                         } }
+    network_firewall_policy_id = oci_network_firewall_network_firewall_policy.these[each.value.policy_key].id
+    action                     = each.value.action
+    name                       = each.value.name
+    decryption_profile         = each.value.decryption_profile_id != null ? oci_network_firewall_network_firewall_policy_decryption_profile.these["${each.value.policy_key}.${each.value.decryption_profile_id}"].id : null
+    secret                     = each.value.secret
+
+    condition {
+      destination_address = each.value.destination_ip_address_list != null ? [oci_network_firewall_network_firewall_policy_address_list.these["${each.value.policy_key}.${each.value.destination_ip_address_list}"].name] : null
+      source_address      = each.value.source_ip_address_list != null ? [oci_network_firewall_network_firewall_policy_address_list.these["${each.value.policy_key}.${each.value.source_ip_address_list}"].name] : null
+    }
+
+    position {
+      after_rule = 1
+    }
+}
+
+/* resource "oci_network_firewall_network_firewall_policy" "these" {
 
   for_each = local.one_dimension_processed_nfw_policies != null ? length(local.one_dimension_processed_nfw_policies) > 0 ? local.one_dimension_processed_nfw_policies : {} : {}
 
@@ -270,4 +415,4 @@ resource "oci_network_firewall_network_firewall_policy" "these" {
       }
     }
   }
-}
+} */
