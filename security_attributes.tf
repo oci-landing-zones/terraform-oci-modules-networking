8d856098d9785e5482e7280926584196dcd7ec81
Erna Guerrero <erna.guerrero@oracle.com>
2024-10-23 21:11:22 -0600

    modified validation check for security attributes

--- security_attributes.tf
+++ security_attributes.tf
@@ -36,14 +36,42 @@ locals {
     ]) : flat_security.security_attr_key => flat_security
   } : null
 
+  ### data source list of security attributes in tenancy
   secattr_list = length(data.oci_security_attribute_security_attributes.these) > 0 ? flatten([
     for k, v in data.oci_security_attribute_security_attributes.these : [
       for i in v.security_attributes : {
-        key       = i.security_attribute_namespace_name
         namespace = i.security_attribute_namespace_name
         attr_name = i.name
       }
     ]
   ]) : []
-  secattr_list_by_name = { for i in local.secattr_list : "${i.key}.${i.attr_name}" => { namepace = i.namespace, attr_name = i.attr_name } }
+  secattr_list_by_name = [ for i in local.secattr_list : "${i.namespace}.${i.attr_name}" ]
+
+  vcn_secattr_name_list = local.vcn_security_attrs != null ? {
+    for vcn_secattr in flatten([
+      for k, v in local.vcn_security_attrs : [
+        {
+          names = v.zpr_attributes != null ? [
+            for a in v.zpr_attributes :
+              "${a.namespace}.${a.attr_name}"
+          ] : []
+          secattr_list_key = k
+        }
+      ]
+    ]) : vcn_secattr.secattr_list_key => vcn_secattr
+  } : null
+
+  ### used to generate list of missing security attribute names
+  missing_secattrs = local.vcn_secattr_name_list != null ? {
+    for missing in flatten([
+      for k, v in local.vcn_secattr_name_list : [
+      {
+        missing_ns_attr_names = v.names != null ? [
+            join(", ", [for a in setsubtract(toset(v.names), (toset(local.secattr_list_by_name))) : format("\"%s\"", a)])
+        ] : []
+        secattrs_key = k
+        }
+      ]
+    ]) : missing.secattrs_key => missing
+  } : null
 }
